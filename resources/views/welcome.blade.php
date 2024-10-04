5f7fc61489c2ac4a9b4953729a6f1133278cb537
CARLOS MIGUEL PAREJA <PCN0587@dlsud.edu.ph>
2024-10-04 14:04:52 +0800

    tailwind added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
 </head>
 <body>
     <h1>Welcome</h1>
+    <div class="text-center text-2xl font-bold">
+        Welcome to Laravel with Tailwind CSS!
+    </div>
+
 </body>
 </html>
 
