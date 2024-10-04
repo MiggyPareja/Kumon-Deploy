d169ac9fd771cdd625ffb7fcec5484b82898f270
CARLOS MIGUEL PAREJA <PCN0587@dlsud.edu.ph>
2024-10-04 15:25:07 +0800

    Revert "tailwind added"
    This reverts commit 5f7fc61489c2ac4a9b4953729a6f1133278cb537.

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./resources/views/**/*.blade.php", "./resources/js/**/*.js"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
